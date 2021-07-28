439fbb80ec71081acb29601944367c98e83e2ebf
Niv <nivlev3@gmail.com>
2021-07-28 17:54:03 +0300

    V

--- src/adminComps/EditProduct.js
+++ src/adminComps/EditProduct.js
@@ -6,13 +6,14 @@ import { useEffect } from 'react';
 import { toast } from 'react-toastify';
 
 function EditProducts({urlParam}){
-    const { register, handleSubmit, setValue, formState: { errors } } = useForm();
+    const { register, handleSubmit,setValue, formState: { errors } } = useForm();
     const history = useHistory()
     const name = register("name", { required: true, minLength: 2 });
     const price = register("price", { required: true, minLength: 1 });
     const category = register("category", { required: true, minLength: 2 });
     const image = register("image", { required: true, minLength: 10 });
-    let [oldProd,setOldProd] = useState({})
+    let [oldProd,setOldProd] = useState({});
+    const [showProd,setShowProd] = useState({...oldProd})
     useEffect(()=>{
         const getProdDetails = async() =>{
             let url = API_URL + '/products/single/' + urlParam;
@@ -46,37 +47,54 @@ function EditProducts({urlParam}){
             toast.warning("There was an error updating, try again later")
         }
     }
+    const setShow = (e) =>{
+        console.log("work");
+        // console.log(e.target.value);
+        // setShowProd({...showProd,key:value})
+    }
     return(
         <form onSubmit={handleSubmit(onSub)}>
         <div className="container">
+            <div className="row">
+            <div className="product-card">
+        <div className="product-tumb">
+            <img src={oldProd.image} alt=""/>
+        </div>
+        <div className="product-details">
+            {/* <span class="product-catagory">Women,bag</span> */}
+            <h4>{oldProd.name}</h4>
+            <div className="product-bottom-details">
+                <div className="product-price">{oldProd.price}$</div>
+                <div className="product-links"> 
+            </div></div></div></div>
+            </div>
             <div className="row">
         <div className="input-wrapper twoInputsInLine">
         <div className="input-data">
           <input {...name} type="text" className="text-primary"  />
           <label className="text-warning mb-1">Name:</label>
-          <div class="underline"></div>
+          <div className="underline"></div>
         </div>
         </div>
         <div className="input-wrapper twoInputsInLine">
         <div className="input-data">
           <input {...price} type="number" className="text-primary" />
           <label className="text-warning mb-1">Price:</label>
-          <div class="underline"></div>
+          <div className="underline"></div>
         </div>
         </div>
         <div className="input-wrapper twoInputsInLine">
         <div className="input-data">
           <input {...category}  type="text" className="text-primary"  />
           <label  className="text-warning mb-1">Category:</label>
-          <div class="underline"></div>
+          <div className="underline"></div>
         </div>
         </div>
-
         <div className="input-wrapper w-75">
         <div className="input-data">
           <input {...image} type="text" className="text-primary"  />
           <label className="text-warning mb-1">Image url:</label>
-          <div class="underline"></div>
+          <div className="underline"></div>
         </div>
         </div>
         <div className="text-center">
