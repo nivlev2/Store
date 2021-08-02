7c3fbced24bb5dea85a623faf7eec93d021f2657
Niv <nivlev3@gmail.com>
2021-08-02 11:21:10 +0300

    change api

--- src/services/apiSer.js
+++ src/services/apiSer.js
@@ -1,7 +1,5 @@
 import axios from "axios"
-// "https://shopniv.herokuapp.com"
-export const API_URL = "http://localhost:3400" ;
-// export const PER_PAGE = 5;
+export const API_URL = "https://shopniv.herokuapp.com" ;
 
 export const doApiGet = async (_url) => {
   try {
